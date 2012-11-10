f0514685c760378d73f2776a60f0c3273f8fac44
Brian Gardner <brngardner@gmail.com>
2012-11-10 13:13:50 -0500

    Derp, forgot to remove pry statements

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -2,12 +2,10 @@ class UserRegistrationsController < Devise::RegistrationsController
   def new
     @user = User.new(params[:user])
     @user.initialize_rolable_type
-    binding.pry
   end
 
   def create
     @user = User.new(params[:user])
-    binding.pry
 
     if @user.save
       @user.create_child_class
