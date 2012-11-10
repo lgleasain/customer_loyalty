20f1f80f26913ecf604bfe0cb0d1c77436e6adec
Brian Gardner <brngardner@gmail.com>
2012-11-10 11:17:05 -0500

    Add merchant and client customer models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110153003) do
+ActiveRecord::Schema.define(:version => 20121110155059) do
+
+  create_table "customers", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "merchants", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
