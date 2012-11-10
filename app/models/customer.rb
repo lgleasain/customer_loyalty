3c15e89ba3b9313267b863938f5c1ca016608d1b
Brian Gardner <brngardner@gmail.com>
2012-11-10 17:48:36 -0500

    add dependent destroy to user subclasses

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
-  has_one :user, as: :rolable
+  has_one :user, as: :rolable, dependent: :destroy
   has_many :customer_passbooks
   has_many :merchants
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,12 @@
 class Merchant < ActiveRecord::Base
-  has_one :user, as: :rolable
+  has_one :user, as: :rolable, dependent: :destroy
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses
   attr_accessible :addresses_attributes, :about
 
   validates :addresses, length: { minimum: 1 }
+
+  def user
+    User.where(rolable_type: 'merchant', rolable_id: self.id).first
+  end
 end
