29be3a27f076e51c5a6fe17cf8b0f674980ef49a
Brian Gardner <brngardner@gmail.com>
2012-11-10 13:38:02 -0500

    Add address model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110180556) do
+ActiveRecord::Schema.define(:version => 20121110181908) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "street"
+    t.string   "street_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "customers", :force => true do |t|
     t.datetime "created_at", :null => false
