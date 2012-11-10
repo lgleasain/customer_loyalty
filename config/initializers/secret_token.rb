b6552f4cefd763401e346710fafe352e3bba382d
Lance Gleason <lgleasain@yahoo.com>
2012-11-10 00:06:05 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerLoyalty::Application.config.secret_token = 'df0dd070dded87b7beff863329552324055a297bf2e7e0cf578950310b0cf0a2126d3700e992d343fe44c0da122aa243bab56ee6d757b22fb9f4b0056b0bda76'
