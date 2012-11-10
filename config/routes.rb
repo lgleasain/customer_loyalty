574c5449f189fefe3a3b274ea36f1710ebd921f9
Brian Gardner <brngardner@gmail.com>
2012-11-10 13:14:22 -0500

    Use form partial for user signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CustomerLoyalty::Application.routes.draw do
   devise_for :users, controllers: { registrations: 'UserRegistrations' }
-  devise_scope :users do
+  resources :users
+  devise_scope :user do
     match 'merchant/sign_up' => 'user_registrations#new', :user => { :rolable_type => 'merchant' }
     match 'customer/sign_up' => 'user_registrations#new', :user => { :rolable_type => 'customer' }
   end
