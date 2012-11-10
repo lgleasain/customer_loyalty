96aedec0f5e8331394dcae57a48b3d7e6b822c6f
Brian Gardner <brngardner@gmail.com>
2012-11-10 12:55:10 -0500

    Update user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 CustomerLoyalty::Application.routes.draw do
   devise_for :users, controllers: { registrations: 'UserRegistrations' }
-  devise_scope :user do
+  devise_scope :users do
     match 'merchant/sign_up' => 'user_registrations#new', :user => { :rolable_type => 'merchant' }
     match 'customer/sign_up' => 'user_registrations#new', :user => { :rolable_type => 'customer' }
   end
-  resources :users
 
   root :to => "home#index"
 end
