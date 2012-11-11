ecb00ae00bd8395bbf02d31fb68157401f18e57b
William Metz <awillimus@gmail.com>
2012-11-10 19:28:22 -0500

    filling in track_visit with real values from model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ActiveRecord::Base
   has_one :user, as: :rolable, dependent: :destroy
   has_many :customer_passbooks
   has_many :merchants
+
+  def user
+    User.where(rolable_type: 'customer', rolable_id: self.id).first
+  end
 end
+
