cfd246f890194b1483c8bd65506d30b5423d98e7
Brian Gardner <brngardner@gmail.com>
2012-11-10 21:08:59 -0500

    Fix merchant and customer factories so users are correctly created

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :customer do
-    user {create :user}
+    after(:create) do |customer|
+      user = create :user
+      user.rolable_id = customer.id
+      user.rolable_type = 'customer'
+      user.save
+    end
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
   factory :merchant do
-    sequence(:name) {|n| "The Honey Badger #{n} "}
     sequence(:reward_program_name) {|n| "Cobra Rewards #{n}"}
     sequence(:earn_type) {|n| "Cobras #{n}"}
     sequence(:reward_threshold_number) {|n| n}
     sequence(:reward_descripton) {|n| "Bee Larvae #{n}"}
+    addresses { create_list :address, 2 }
+
+    after(:create) do |merchant|
+      user = create :user
+      user.rolable_id = merchant.id
+      user.rolable_type = 'merchant'
+      user.save
+    end
   end
 end
